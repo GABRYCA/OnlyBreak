ffd5383e9f64d5fbb736d9a3b63cea99a2756cc5
GABRYCA <gabrycaretti@gmail.com>
2019-07-20 10:56:06 +0200

    Versione 1.0.1
    Modifiche:
    - Aggiunto un if che verifica se il nome del blocco che si sta aggiungendo con /add è veramente un blocco, nel caso non lo fosse, lo dice al player.

--- src/it/gabryca/onlybreak/AddBlock.java
+++ src/it/gabryca/onlybreak/AddBlock.java
@@ -1,6 +1,7 @@
 package it.gabryca.onlybreak;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,16 +18,21 @@ public class AddBlock implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
         if (commandSender.hasPermission(Main.getInstance().getConfig().getString("AddBlockPermission"))) {
             if (strings.length == 2) {
-                commandSender.sendMessage(ChatColor.GREEN + Main.getInstance().getConfig().getString("message.command-correct") + " [ " + strings[0] + " , " + strings[1] + " ]");
-                List<String> blocks = Main.getInstance().getConfig().getStringList("blocks");
-                List<String> permissions = Main.getInstance().getConfig().getStringList("permissions");
-                blocks.add(strings[0]);
-                Main.getInstance().getConfig().set("blocks", blocks);
-                permissions.add(strings[1]);
-                Main.getInstance().getConfig().set("permissions", permissions);
-                Main.getInstance().saveConfig();
+                if(!(Material.getMaterial(strings[0]) == null)) {
+                    commandSender.sendMessage(ChatColor.GREEN + Main.getInstance().getConfig().getString("message.command-correct") + " [ " + strings[0] + " , " + strings[1] + " ]");
+                    List<String> blocks = Main.getInstance().getConfig().getStringList("blocks");
+                    List<String> permissions = Main.getInstance().getConfig().getStringList("permissions");
+                    blocks.add(strings[0]);
+                    Main.getInstance().getConfig().set("blocks", blocks);
+                    permissions.add(strings[1]);
+                    Main.getInstance().getConfig().set("permissions", permissions);
+                    Main.getInstance().saveConfig();
+                } else {
+                    commandSender.sendMessage(ChatColor.RED + Main.getInstance().getConfig().getString("message.warn-NotMaterial") + " [ " +strings[0] + " ]");
+                }
             } else {
                 commandSender.sendMessage(ChatColor.RED + Main.getInstance().getConfig().getString("message.warn-format"));
             }
